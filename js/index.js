97451a82d4fe22bdb27a5d9c4df01d8b7c2e1079
uriasvitor <81401104+uriasvitor@users.noreply.github.com>
2022-10-13 01:01:25 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,18 +4,26 @@ const saveImgBtn = document.querySelector("#btn-save");
 let previewImg = document.querySelector(".preview-img img");
 
 function preview() {
+
     let file = fileInput.files[0];
     previewImg.src = URL.createObjectURL(file);
 
     document.getElementById("previewImg").style.filter = "invert(0)"
     document.getElementById("btn-save").disabled = false;
-    
+
+    const size = document.getElementById("previewImg")
+
+    setTimeout(() => {
+        document.getElementById("largura").value = size.clientWidth;
+        document.getElementById("altura").value = size.clientHeight;
+        
+    }, 500);
 }
 
 document.querySelector(".btn-inversor").addEventListener("click",(e)=>{
     e.preventDefault();
 
-     document.getElementById("previewImg").style.filter = "invert(1)"
+     document.getElementById("previewImg").style.filter = "saturate(0)invert(1)"
    
 
 })
@@ -23,19 +31,19 @@ document.querySelector(".btn-inversor").addEventListener("click",(e)=>{
 const saveImg = (e) =>{
     e.preventDefault()
 
-    let imgWidth = document.getElementById("largura").value;
-    let imgHeight = document.getElementById("altura").value;
+    let imgWidth = document.getElementById("largura").value
+    let imgHeight = document.getElementById("altura").value
 
     console.log(`${imgHeight} ${imgWidth}`)
 
     const canvas = document.createElement("canvas");
     const ctx = canvas.getContext("2d");
-
+    
     canvas.width = imgWidth;
     canvas.height = imgHeight;
     console.log("create canvas")
     
-    ctx.filter = 'invert(1)'
+    ctx.filter = 'saturate(0)invert(1)'
     ctx.drawImage(previewImg, 0, 0, canvas.width, canvas.height)
     ctx.drawImage(previewImg, -canvas.width / 100, -canvas.height / 100, canvas.width, canvas.height);
 
